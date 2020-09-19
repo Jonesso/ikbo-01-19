f8f3729989670401b27fff510d5060be5524582b
Sophie P <sonya.pav25@gmail.com>
2020-09-19 13:56:52 +0300

    Practice 8

---
+++ practice8 - Pavlinina/src/ru/mirea/pr8/UnfairWaitList.java
@@ -0,0 +1,26 @@
+package ru.mirea.pr8;
+
+/**
+ * @param <E> - generic type for list
+ */
+public class UnfairWaitList<E> extends WaitList<E> {
+
+    public UnfairWaitList() {
+        super();
+    }
+
+    /**
+     * @return the removed element
+     */
+    @Override
+    public E remove() {
+        return super.remove();
+    }
+
+    /**
+     * @param element add to end of list
+     */
+    public void moveToBack(E element) {
+        super.add(element);
+    }
+}
